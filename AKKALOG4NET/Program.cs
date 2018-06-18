64307222f9d357041d5092316548928e7690c6d6
matt goodman <matt.goodman@gmail.com>
2018-06-18 21:58:23 +1000

    adding test

---
+++ AKKALOG4NET/Program.cs
@@ -0,0 +1,23 @@
+﻿using Akka.Actor;
+using log4net;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AKKALOG4NET
+{
+    class Program
+    {
+        static ILog log = LogManager.GetLogger(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType);
+        static void Main(string[] args)
+        {
+            log4net.Config.XmlConfigurator.Configure();
+            var sys = ActorSystem.Create("test");
+            log.Debug("log");
+            Console.WriteLine("waiting");
+            Console.ReadLine();
+        }
+    }
+}
